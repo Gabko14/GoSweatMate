ff92a9c0b76f9c1768814d27906417a128af9464
Zhubi Nis <nis.zhubi@bbcag.ch>
2023-05-17 15:50:46 +0200

    Api

--- app/src/main/java/ch/bbcag/gosweatmate/MainActivity.java
+++ app/src/main/java/ch/bbcag/gosweatmate/MainActivity.java
@@ -3,6 +3,16 @@ package ch.bbcag.gosweatmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text69);
+        String url = "https://wger.de/api/v2/exercise/";
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println(response);
+                        textView.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(stringRequest);
     }
+
+
 }
